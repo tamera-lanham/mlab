ea282a8fdd42f8ce909014d9ad0f12a73b8d22c7
Tamera Lanham <tamera.lanham@gmail.com>
2022-02-15 16:25:32 -0800

    clone via ssh

--- lambda-labs-setup.sh
+++ lambda-labs-setup.sh
@@ -1,6 +1,6 @@
 conda deactivate
 cd ~;
-git clone https://github.com/tamera-lanham/mlab.git
+git clone git@github.com:tamera-lanham/mlab.git
 cd mlab
 git config user.email "tamera.lanham@gmail.com"
 git config user.name "Tamera Lanham"
