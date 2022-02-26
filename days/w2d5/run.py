279357493f2a92f8e8e33b75ef1d4a7e9dbc5bd7
Tamera Lanham <tamera.lanham@gmail.com>
2022-02-26 23:41:26 +0000

    2/25/2022

--- days/w2d5/run.py
+++ days/w2d5/run.py
@@ -1,3 +1,4 @@
+import einops
 import math
 import os
 import torch as t
@@ -38,11 +39,64 @@ def corpus_to_tensor_list(corpus, max_tokens=1024):
         
     return tensor_list
 
-def to_batches(list_of_tensors, batch_size):
-    n_batches = math.ceil(len(list_of_tensors) / batch_size)
-    batches = [list_of_tensors[i:i+batch_size] for i in range(n_batches)]
-    return batches
 
+################################################################
+
+# These functions (corpus_jsonl, corpus_tokens, corpus_to_tensors)
+# work together to load in data a little bit at a time with generators
+
+def corpus_jsonl(filename='lw_corpus.jsonl'):
+    with open(filename) as f:
+        for line in f:
+            yield json.loads(line.strip())
+    
+def corpus_tokens(corpus, tokenizer):
+    for d in corpus:
+        yield tokenizer(d['title'] + d['text'] + '<|endoftext|>').input_ids
+
+def buffer(iterable_of_lists):
+    iterator = iter(iterable_of_lists)
+    buffer_storage = []
+    
+    def add_to_buffer():
+        nonlocal buffer_storage
+        try: buffer_storage += next(iterator)
+        except: return True
+    
+    def get(n):
+        nonlocal buffer_storage
+        while len(buffer_storage) < n:
+            done = add_to_buffer()
+            if done: break
+            
+        n_elements, buffer_storage = buffer_storage[:n], buffer_storage[n:]
+        return n_elements
+    
+    return get
+
+def corpus_to_tensors(corpus, max_tokens=1024, include_last_handful=False):
+    tokenizer = transformers.GPT2TokenizerFast.from_pretrained("gpt2")
+    token_buffer = buffer(corpus_tokens(corpus, tokenizer))
+    
+    while True:
+        tokens = token_buffer(max_tokens)
+        
+        break_condition = (tokens == []) if include_last_handful else (len(tokens) < max_tokens)
+        if break_condition: break
+        
+        yield t.tensor(tokens)
+        
+        
+def to_batches(tensors_iter, batch_size):
+    tensors_iter = iter(tensors_iter)
+    
+    while True:
+        try: batch = t.stack([next(tensors_iter) for _ in range(batch_size)])
+        except: break
+        yield batch
+
+################################################################        
+        
 class DistributedDataLoader():
     def __init__(
         self, 
@@ -66,9 +120,10 @@ class DistributedDataLoader():
         if rank == 0:
             # 2. If in charge, then load all the training data
             self.corpus = import_lesswrong_corpus()
-            list_of_tensors = corpus_to_tensor_list(self.corpus, self.max_tokens)
-            self.batches = to_batches(list_of_tensors, self.batch_size)
+            list_of_tensors = list(corpus_to_tensors(self.corpus, self.max_tokens))            
+            self.batches = list(to_batches(list_of_tensors, self.batch_size))
             self.n_batches = len(self.batches)
+            
             print('I am rank 0, and I know that there are %d batches' % self.n_batches)
             
         else: 
@@ -89,8 +144,7 @@ class DistributedDataLoader():
             for i in range(self.n_batches):
                 
                 if self.rank == 0:
-                    batch_tensor_list = self.batches[i]
-                    batch = t.stack(batch_tensor_list).to(self.gpu) # will have shape (self.batch_size, self.max_tokens)
+                    batch = self.batches[i].to(self.gpu) # will have shape (self.batch_size, self.max_tokens)
                 
                 else:
                     batch = t.zeros((self.batch_size, self.max_tokens), dtype=t.int64).to(self.gpu)
@@ -104,7 +158,10 @@ class DistributedDataLoader():
                 
         return generator()
 
-# cd mlab/days/w2d5/ && python run.py
+mem = lambda rank: t.cuda.memory_allocated('cuda:%d' % rank) / 2**(30) 
+
+#print(mem())
+
 def run(rank, size):
     # tensor = t.zeros(3,3)
     group = dist.new_group(list(range(size)))
@@ -117,17 +174,27 @@ def run(rank, size):
     
     loss_vals, accs = [], []
     running_loss = 0
+    counter = 0
     
-    for minibatch in dataloader: # first 80% are train, rest are val
-        train = minibatch[:math.floor(mini_batch_size*0.8),:]
+    for minibatch in dataloader: # first 80% are train, rest are val 
+        counter += 1
+        train = minibatch[:math.floor(mini_batch_size*0.8),:] # will have shape (mini_batch_size, max_tokens)
         val = minibatch[math.floor(mini_batch_size*0.8):,:]
         
-        optimizer.zero_grad()
-        _, outputs = gpt2(train)
+        inputs, labels = train[:,:-1], train[:,1:]
         
-        loss = loss_func(outputs, train)
+        optimizer.zero_grad() # torch.Size([12, 1024])
+
+        outputs = gpt2(inputs).logits # torch.Size([12, 1024, 50257])
+        print(f'Rank: {rank}, batch: {counter}, memory: {mem(rank)} GiB')
+        vocab_size = outputs.shape[-1]
+
+        outputs_rearranged = einops.rearrange(outputs, 'mb tokens vocab -> (mb tokens) vocab')
+        labels_rearranged = einops.rearrange(labels, 'mb tokens -> (mb tokens)')
+        
+        loss = loss_func(outputs_rearranged, labels_rearranged)
         running_loss += loss.detach()
-        mem_usage = mem() # moment of highest GPU usage
+        loss_vals += [loss.detach()]
         loss.backward()
         
         # Shares the gradients and average it out
@@ -138,20 +205,22 @@ def run(rank, size):
         
         optimizer.step()
         
+# cd /home/ubuntu/mlab/days/w2d5/ && python run.py
         
-        if i % training_config['val_batches'] == 0:
-            with t.no_grad():
+    print(loss_vals)
+#         if i % training_config['val_batches'] == 0:
+#             with t.no_grad():
 
-                test_targets, test_inputs = zip(*train_batch)
-                test_targets = t.Tensor(test_targets).to(device)
-                test_inputs = t.stack(test_inputs).to(device)
+#                 test_targets, test_inputs = zip(*train_batch)
+#                 test_targets = t.Tensor(test_targets).to(device)
+#                 test_inputs = t.stack(test_inputs).to(device)
 
-                _, test_outputs = bert(test_inputs)
-                accs.append(float(acc(test_outputs, test_targets).cpu().detach()))
-                loss_vals.append(running_loss / training_config['val_batches'])
-                running_loss = 0.0 
+#                 _, test_outputs = bert(test_inputs)
+#                 accs.append(float(acc(test_outputs, test_targets).cpu().detach()))
+#                 loss_vals.append(running_loss / training_config['val_batches'])
+#                 running_loss = 0.0 
 
-                data.set_postfix({'acc': sum(accs[-5:]) / min(len(accs), 5), 'loss': float(loss_vals[-1]), 'gpu usage': f'{mem_usage:.1f} GiB'})
+#                 data.set_postfix({'acc': sum(accs[-5:]) / min(len(accs), 5), 'loss': float(loss_vals[-1]), 'gpu usage': f'{mem_usage:.1f} GiB'})
                 
     # for param in model.parameters():
         # param.grad          
