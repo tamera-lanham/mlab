43808fb29963c2daa12f087f2d4e2c484938d149
Tamera Lanham <tamera.lanham@gmail.com>
2022-03-03 01:38:59 +0000

    3/2

--- days/w3d1/run-old-2.py
+++ days/w3d1/run-old-2.py
@@ -23,11 +23,11 @@ def label_to_tensor(label):
     else: 
         return t.Tensor([1, 0])
 
-def preprocess_batch(batch, gpu):
+def preprocess_batch(batch):
     labels, inputs = zip(*batch)
-    labels = t.stack([label_to_tensor(label) for label in labels]).to(gpu)
-    return labels, inputs
-
+    labels = t.stack([label_to_tensor(label) for label in labels])
+    return (labels, inputs)
+    
 def get_batches(rank, gpu, fake=True, batch_size=32, max_seq_len=512, n_fake_batches=10):
     
     if rank == 0:
@@ -35,8 +35,8 @@ def get_batches(rank, gpu, fake=True, batch_size=32, max_seq_len=512, n_fake_bat
         if fake: train_batches, test_batches = fake_imdb_data(batch_size, max_seq_len, n_fake_batches)
         else: train_batches, test_batches = imdb_data(batch_size, max_seq_len)
         
-        train_batches = [preprocess_batch(batch, gpu) for batch in train_batches]
-        test_batches = [preprocess_batch(batch, gpu) for batch in test_batches]
+        train_batches = [preprocess_batch(batch) for batch in train_batches]
+        test_batches = [preprocess_batch(batch) for batch in test_batches]
         
         n_batches = len(train_batches)
         seq_len = int(train_batches[0][1][1].shape[0])
@@ -75,16 +75,6 @@ def prepare_process(rank, size, all_groups): # Loads the model and gets the grou
 
     return ProcessInfo(rank, size, model, gpu, next_group, prev_group)
 
-def pass_labels_to_last(p_info, labels, group, label_shape = (32, 2)):
-
-    if p_info.rank == 0:
-        dist.broadcast(labels, 0, group)
-        
-    if p_info.rank == p_info.size - 1: # If you are the last process
-        labels = t.zeros(label_shape, device=p_info.gpu)
-        dist.broadcast(labels, 0, group)
-        
-    return labels
 
 def forward_pass_microbatch(p_info, microbatch=None, middle_shape=(1, 512, 4096)):
     
@@ -94,7 +84,7 @@ def forward_pass_microbatch(p_info, microbatch=None, middle_shape=(1, 512, 4096)
     if not prev_group: # When this is the first model in the chain (rank 0)
         input_data = microbatch        
     else:
-        input_data = t.zeros(middle_shape, device=gpu)
+        input_data = t.zeros(middle_shape).to(gpu)
         dist.broadcast(input_data, rank-1, prev_group)
         
     # Do the forward pass
@@ -107,28 +97,87 @@ def forward_pass_microbatch(p_info, microbatch=None, middle_shape=(1, 512, 4096)
     else: # When this is the last model in the chain
         return output_data
     
-def forward_pass_batch(p_info, data_batch, n_microbatches, microbatch_size, seq_len): # bata_batch has data when rank==1, otherwise it's None
+def back_pass_microbatch(p_info, loss_for_microbatch, optimizer, middle_shape=(1, 512, 4096)): 
+    # loss_for_microbatch has data when rank==3, otherwise it's None
+    (rank, size, model, gpu, next_group, prev_group) = p_info.as_tuple()
+    
+    optimizer.no_grad()
+    
+    # -1. Receive the loss if 3 (because loss has been pre-calculated for you)
+    if rank == 3: 
+        # make a holder of the broadcasted results somewhere here
+        intermediate_gradient = loss_for_microbatch
+        intermediate_gradient.backward() # ???
+        dist.broadcast(intermediate_gradient, prev_group)
+        
+    elif rank != 3: 
+        gradient_holder = torch.zeros(middle_shape).to(gpu)
+        dist.broadcast(gradient_holder, next_group)
+        # You, because you are not CUDA:3, you shoudl calculate your own losses
+        some_kinda_of_operations(gradient_holder, something).backward()
+    
+    # 2. if you are rank 0. you still backward but you are done
+    # if rank == 0: 
+        # anything extra?
+    return None
+    
+    
+def back_pass_batch(process_info, forward_pass_outputs, loss_microbatched, optimizer, n_microbatches, microbatch_size, seq_len): # loss_microbatched has data when rank==3, otherwise it's None
+    
+    # Somewhere in here:
+    #
+    for loss_for_microbatch in loss_microbatched:
+        back_pass_microbatch(p_info, loss_for_microbatch, optimizer, middle_shape)
+    
+    raise NotImplmentedError()
+    
+def forward_pass_batch(p_info, data_batch, n_microbatches, microbatch_size, seq_len, loss_fn, train_labels): # bata_batch has data when rank==1, otherwise it's None
     
     middle_shape = (microbatch_size, seq_len, 4096)
     
     microbatches = batch_to_microbatches(data_batch, n_microbatches, microbatch_size) if (data_batch is not None) else None
     
-    final_outputs = []
+    outputs, losses = [], []
+    
     for i in range(n_microbatches):
         
         microbatch = microbatches[i].to(p_info.gpu) if microbatches else None
-                
+        
+        print(f'rank {p_info.rank}, microbatch {i}, gpu usage {mem(p_info.rank):.2f} GiB (pre-forward pass)')
+        
         output_data = forward_pass_microbatch(p_info, microbatch, middle_shape)
+        # Calculate loss and do back pass
+        
         print(f'rank {p_info.rank}, microbatch {i}, gpu usage {mem(p_info.rank):.2f} GiB')
         
         if output_data is not None:
-            final_outputs.append(output_data.detach())
+            outputs.append(output_data.detach())
+            
              # This .detach() prevents a GPU memory overflow - but we might actually need to keep the gradients around for the backward pass 😬 
             
-    if final_outputs:
-        return t.cat(final_outputs, 0)
+    if outputs and train_labels:
+        return outputs, train_labels
     
+# if p_info.rank == 0:
+#     group = dist.new_group([0, 3])
+#     dist.broadcast(train_labels, 0, group, async_op=True)
+# if p_info.rank == 3: 
+#     group = dist.new_group([0, 3])
+#     train_labels = t.zeros(n_microbatches * microbatch_size, 2)
+#     dist.broadcast(train_labels, 0, group, async_op=True)
+def pass_labels_to_last(p_info, labels, group, label_shape = (32, 2)):
 
+    if p_info.rank == 0:
+        dist.broadcast(labels, 0, group)
+        
+    if p_info.rank == p_info.size - 1: # If you are the last process
+        labels = t.zeros(label_shape, device=p_info.gpu)
+        dist.broadcast(labels, 0, group)
+        
+    return labels
+        
+    
+    
 # cd /home/ubuntu/mlab/days/w3d1 && python run.py
 def run(rank, size, all_groups): 
     
@@ -141,24 +190,34 @@ def run(rank, size, all_groups):
     
     n_batches, seq_len, (train, test) = get_batches(rank, process_info.gpu, fake=True, batch_size=batch_size)
     
+    
     for i in range(n_batches):
-        
         
         train_labels, train_inputs = train[i] if rank == 0 else (None, None)
         test_labels, test_inputs  = test[i] if rank == 0 else (None, None)
         
+        #train_labels = pass_labels_to_last(process_info, train_labels, all_groups['first_and_last'], (batch_size, 2)) # Now rank 3 will have a tensor for train_labels
+        #test_labels = pass_labels_to_last(process_info, test_labels, all_groups['first_and_last'], (batch_size, 2))
         
-        train_labels = pass_labels_to_last(process_info, train_labels, all_groups['first_and_last'], (batch_size, 2))
-        test_labels = pass_labels_to_last(process_info, test_labels, all_groups['first_and_last'], (batch_size, 2))
+        #if rank == 3:
+        #    print(train_labels.shape)
         
-        if rank == size - 1:
-            print(f'! {train_labels.shape}, {test_labels.shape}')
-                        
-        forward_pass_outputs = forward_pass_batch(process_info, train_inputs, n_microbatches, microbatch_size, seq_len)
+        # Calculate loss
+        # Rank 3 needs to have labels here
+        loss_fn = t.nn.MSELoss()
         
-        if forward_pass_outputs is not None: print(forward_pass_outputs.shape)
+        forward_pass_outputs, train_labels = forward_pass_batch(process_info, train_inputs, n_microbatches, microbatch_size, seq_len, loss_fn, train_labels)
+        
+        print(forward_pass_outputs, train_labels)
         
         break
+        # Do backwards pass
+#         backward_pass_results = back_pass_batch(process_info, forward_pass_outputs, loss, optimizer, n_microbatches, microbatch_size, seq_len)
+        
+#         if forward_pass_outputs is not None: print(forward_pass_outputs.shape)
+        
+#         optimizer.step()
+
 
 def init_processes(rank, size, fn, backend='gloo'):
     """ Initialize the distributed environment. """
